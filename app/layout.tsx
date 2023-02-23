76bcedb97d8f068803b1b749eb52945d42afba37
Victor F. Santos <victor.gaard@gmail.com>
2023-02-23 11:33:14 +0100

    feat: SEO improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,44 @@ import { Inter } from '@next/font/google';
 
 const font = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+  title: {
+    default: 'Saw that band',
+    template: '%s | Saw that band'
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  },
+  twitter: {
+    title: 'Saw that band',
+    card: 'summary_large_image'
+  },
+  icons: {
+    shortcut: '/favicon.ico'
+  },
+  verification: {
+    google:
+      'add from here later on https://search.google.com/search-console/welcome'
+  }
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={font.className}>
+      <body className="antialiased">
         <div className="flex min-h-screen">
           <div className="flex w-24 shrink-0 items-end justify-center border-r bg-gray-100 pb-8">
             <p className="rotate-180 [writing-mode:vertical-lr]">
