1d1463cf5755507721b7de05ac02e52f0443ab5b
Victor F. Santos <victor.gaard@gmail.com>
2023-08-18 20:50:02 +0200

    feat: remove img optimization

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      // Spotify images
-      {
-        protocol: 'https',
-        hostname: 'i.scdn.co',
-        pathname: '/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.countryflagicons.com',
-        pathname: '/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'guerfzlhzjrpooirzvlf.supabase.co',
-        pathname: '/**'
-      }
-    ]
-  },
-  experimental: {
-    appDir: true
+    unoptimized: true
   }
 };
 
