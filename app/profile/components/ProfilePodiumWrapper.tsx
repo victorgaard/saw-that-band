6c585edbda99c13ea70b512bcf699fefdce0dfe5
Victor F. Santos <victor.gaard@gmail.com>
2023-09-23 18:50:32 +0200

    feat: podium fix victor

--- app/profile/components/ProfilePodiumWrapper.tsx
+++ app/profile/components/ProfilePodiumWrapper.tsx
@@ -6,8 +6,8 @@ import ProfilePodium from './ProfilePodium';
 async function ProfilePodiumWrapper() {
   const bandsId = [
     '5268799a-82df-4932-aa92-254cc3ce6aab',
-    '94ebc7fb-9ade-438b-8e81-79de4706e3ca',
-    '49a09421-b99c-4e3d-a044-252153de4984'
+    '970860c6-3172-4fa6-b50a-53063af12299',
+    '94ebc7fb-9ade-438b-8e81-79de4706e3ca'
   ];
   const bands = [
     {
@@ -16,12 +16,13 @@ async function ProfilePodiumWrapper() {
         'Ronnie James Dio, Tony Iommi, Geezer Butler and Vinny Appice playing the Dio era of Black Sabbath. What else to say?'
     },
     {
-      id: '94ebc7fb-9ade-438b-8e81-79de4706e3ca',
-      reason: 'Played Octavarium entire song. Simply insane!'
+      id: '970860c6-3172-4fa6-b50a-53063af12299',
+      reason:
+        'Beneath the waves (01011001) tour with over 18 singers and Arjen Lucassen himself.'
     },
     {
-      id: '49a09421-b99c-4e3d-a044-252153de4984',
-      reason: 'Last André Matos tour.'
+      id: '94ebc7fb-9ade-438b-8e81-79de4706e3ca',
+      reason: 'Played Octavarium entire song. Simply insane!'
     }
   ];
   const res = await getUser();
