3495c3010b45b7fd9226af852521bd7d6c0e8968
Victor F. Santos <victor.gaard@gmail.com>
2023-10-26 13:42:29 +0200

    fix: name missing

--- app/[band]/[id]/page.tsx
+++ app/[band]/[id]/page.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata({
   const bandId = params.id;
   const res = await getUser();
   const user: User = res[0];
-  const name = user.name.split(' ')[0] || user.username;
+  const name = user.name?.split(' ')[0] || user.username;
   const bandArray = await getBandById(bandId, user.id);
 
   if (!bandArray || !user) return { title: null };
