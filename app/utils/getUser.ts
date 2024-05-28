95d7a9de10b691a570ad5a202dafc99f100a2c3f
Victor F. Santos <victor.gaard@gmail.com>
2024-05-28 15:16:12 +0200

    fix: cache user so it reduces unnecessary server calls

--- app/utils/getUser.ts
+++ app/utils/getUser.ts
@@ -1,10 +1,20 @@
 import { API_URL } from '@/environments/environments';
+import { User } from '@/types/user';
 import { headers } from 'next/headers';
+
+let userCache: User[];
 
 async function getUser() {
   const username = headers().get('host')?.split('.')[0];
-  const res = await fetch(`${API_URL}/api/find-user?username=${username}`);
-  return res.json();
+  const cachedUserHasChanged =
+    userCache && userCache.length > 0 && userCache[0].username !== username;
+
+  if (!userCache || cachedUserHasChanged) {
+    const res = await fetch(`${API_URL}/api/find-user?username=${username}`);
+    userCache = await res.json();
+  }
+
+  return userCache;
 }
 
 export default getUser;
