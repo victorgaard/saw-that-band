24ae50f6c76f3c4e4cee25d09ad092dba2ad4436
Victor F. Santos <victor.gaard@gmail.com>
2024-05-28 15:55:36 +0200

    fix: revert in-memory user cache

--- app/utils/getUser.ts
+++ app/utils/getUser.ts
@@ -2,19 +2,11 @@ import { API_URL } from '@/environments/environments';
 import { User } from '@/types/user';
 import { headers } from 'next/headers';
 
-let userCache: User[];
-
 async function getUser() {
   const username = headers().get('host')?.split('.')[0];
-  const cachedUserHasChanged =
-    userCache && userCache.length > 0 && userCache[0].username !== username;
-
-  if (!userCache || cachedUserHasChanged) {
-    const res = await fetch(`${API_URL}/api/find-user?username=${username}`);
-    userCache = await res.json();
-  }
-
-  return userCache;
+  const res = await fetch(`${API_URL}/api/find-user?username=${username}`);
+  const user: User[] = await res.json();
+  return user;
 }
 
 export default getUser;
